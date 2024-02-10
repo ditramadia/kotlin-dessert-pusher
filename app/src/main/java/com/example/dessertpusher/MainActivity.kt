070f91fe40cd056a1eb85769acfc7e7a49e6bbc8
ditramadia <ditrarizqaamadia@gmail.com>
2024-02-10 17:29:57 +0700

    feat: add game logic

--- app/src/main/java/com/example/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/dessertpusher/MainActivity.kt
@@ -2,10 +2,91 @@ package com.example.dessertpusher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.dessertpusher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var revenue = 0
+    private var dessertsSold = 0
+
+    private lateinit var binding: ActivityMainBinding
+
+    // Dessert Data
+    data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
+
+    // Create a list of all desserts, in order of when they start being produced
+    private val allDesserts = listOf(
+        Dessert(R.drawable.cupcake, 5, 0),
+        Dessert(R.drawable.donut, 10, 5),
+        Dessert(R.drawable.eclair, 15, 20),
+        Dessert(R.drawable.froyo, 30, 50),
+        Dessert(R.drawable.gingerbread, 50, 100),
+        Dessert(R.drawable.honeycomb, 100, 200),
+        Dessert(R.drawable.icecreamsandwich, 500, 500),
+        Dessert(R.drawable.jellybean, 1000, 1000),
+        Dessert(R.drawable.kitkat, 2000, 2000),
+        Dessert(R.drawable.lollipop, 3000, 4000),
+        Dessert(R.drawable.marshmallow, 4000, 8000),
+        Dessert(R.drawable.nougat, 5000, 16000),
+        Dessert(R.drawable.oreo, 6000, 20000)
+    )
+    private var currentDessert = allDesserts[0]
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Use Data Binding to get reference to the views
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.dessertButton.setOnClickListener {
+            onDessertClicked()
+        }
+
+        // Set the TextViews to the right values
+        binding.revenue = revenue
+        binding.amountSold = dessertsSold
+
+        // Make sure the correct dessert is showing
+        binding.dessertButton.setImageResource(currentDessert.imageId)
+    }
+
+    /**
+     * Updates the score when the dessert is clicked. Possibly shows a new dessert.
+     */
+    private fun onDessertClicked() {
+
+        // Update the score
+        revenue += currentDessert.price
+        dessertsSold++
+
+        binding.revenue = revenue
+        binding.amountSold = dessertsSold
+
+        // Show the next dessert
+        showCurrentDessert()
+    }
+
+    /**
+     * Determine which dessert to show.
+     */
+    private fun showCurrentDessert() {
+        var newDessert = allDesserts[0]
+        for (dessert in allDesserts) {
+            if (dessertsSold >= dessert.startProductionAmount) {
+                newDessert = dessert
+            }
+            // The list of desserts is sorted by startProductionAmount. As you sell more desserts,
+            // you'll start producing more expensive desserts as determined by startProductionAmount
+            // We know to break as soon as we see a dessert who's "startProductionAmount" is greater
+            // than the amount sold.
+            else break
+        }
+
+        // If the new dessert is actually different than the current dessert, update the image
+        if (newDessert != currentDessert) {
+            currentDessert = newDessert
+            binding.dessertButton.setImageResource(newDessert.imageId)
+        }
     }
 }
